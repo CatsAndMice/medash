80bc93f6797e7259918132c5c9d8afaa2181908f
lihai <2322873900@qq.com>
2021-07-28 17:21:45 +0800

    修改

--- main.js
+++ main.js
@@ -3,14 +3,6 @@ const StrategyFrom = require("./src/fromTest/StrategyFromTest"),
     Calendar = require('./src/calendar/Calendar'),
     MyStorage = require('./src/webStorage'),
     DebounceAndThrottle = require('./src/debounceAndThrottle/DebounceAndThrottle');
-
-let fn = DebounceAndThrottle.debounce(() => {
-    console.log(122);
-}, 1000, true)
-setInterval(() => {
-    fn();
-}, 2000)
-
 module.exports = {
     StrategyFrom,
     MathTool,
