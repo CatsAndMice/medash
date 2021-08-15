73f84caffee7fc33f8e83c8177da85ff2cfb3052
lihai-boop <2322873900@qq.com>
2021-08-15 10:48:10 +0800

    djjd

---
+++ example/func.js
@@ -0,0 +1,52 @@
+// class Func extends Function {
+//     constructor() {
+//         super();
+//     }
+//     after() {
+//         console.log(this);
+//     }
+// }
+// function fn() {
+//     console.log('fn');
+// }
+// fn.prototype = new Func();
+// console.log(fn.prototype.after);
+
+// function fn() { }
+// fn.prototype.after = () => {
+//     console.log(1);
+// }
+// console.log(fn.after);
+
+class JobLink {
+    constructor(fn) {
+        this.fn = fn;
+        this.next = null;
+    }
+
+    setNext(nextJobLink) {
+        this.next = nextJobLink;
+    }
+
+    possSuccess() {
+        let res = this.fn.apply(this, arguments);
+        if (res) {
+            return this.next && this.next.possSuccess.apply(this.next, arguments);
+        }
+    }
+}
+
+let j1 = new JobLink(() => {
+    console.log(1);
+    return true
+}),
+    j2 = new JobLink(() => {
+        console.log(2);
+        return true;
+    }),
+    j3 = new JobLink(() => {
+        console.log(3);
+    })
+j1.setNext(j2);
+j2.setNext(j3);
+j1.possSuccess();
