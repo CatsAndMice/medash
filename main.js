03fa05c323d01b8b2c9268fea14509b1b86ea7a7
CatsAndMice <lihai_js@163.com>
2021-09-07 19:16:59 +0800

    feat: 增加将数组包裹的对象字段转化成自己想要的字段

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const FromTest = require('./src/fromTest/FromTest'),
     Calendar = require('./src/calendar/Calendar'),
     MyStorage = require('./src/webStorage'),
     MyEvent = require('./src/myEvent/MyEvent'),
+    Format = require('./src/format/format'),
     DebounceAndThrottle = require('./src/debounceAndThrottle/DebounceAndThrottle');
 module.exports = {
     StrategyFrom,
@@ -12,5 +13,6 @@ module.exports = {
     MyStorage,
     MyEvent,
     FromTest,
-    DebounceAndThrottle
+    DebounceAndThrottle,
+    Format
 }
---
+++ src/format/format.js
@@ -0,0 +1,21 @@
+const { _error } = require('../tool')
+
+class Format {
+  format (data = [], ...params) {
+    if (!Array.isArray(data)) {
+      _error('参数类型错误,仅支持数组类型!')
+      return
+    }
+    params = params.flat(Infinity)
+    return data.map(val => {
+      const formatObj = {}
+      params.forEach(param => {
+        const { attrName, formatName } = param
+        formatObj[formatName] = val[attrName]
+      })
+      return formatObj
+    })
+  }
+}
+
+module.exports = new Format()
