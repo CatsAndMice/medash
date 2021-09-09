827e8b700b7422c303f15ef121961bac3a1d24fa
li hai <2322873900@qq.com>
2021-09-09 15:02:37 +0800

    test: 测试git

--- src/format/format.js
+++ src/format/format.js
@@ -1,16 +1,16 @@
 const { _error } = require('../tool')
 
 class Format {
-  format (data = [], ...params) {
+  format (data = [], ...param) {
     if (!Array.isArray(data)) {
       _error('参数类型错误,仅支持数组类型!')
       return
     }
     
-    params = params.flat(Infinity)
+    param = param.flat(Infinity)
     return data.map(val => {
       const formatObj = {}
-      params.forEach(param => {
+      param.forEach(param => {
         const { attrName, formatName } = param
         formatObj[formatName] = val[attrName]
       })
