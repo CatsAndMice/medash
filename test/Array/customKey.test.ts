a3b8824738137ac201eeff6f4ac4d3f909cd3282
CatsAndMice <lihai_js@163.com>
2021-12-25 20:45:30 +0800

    test: 测试自动同步gitee

--- test/Array/customKey.test.ts
+++ test/Array/customKey.test.ts
@@ -5,7 +5,6 @@ describe("customKey Object", () => {
         expect(customKey(origin, { key: 'name', customKey: 'sex' })).toEqual([{ sex: 'a' }])
         expect(customKey(origin, { key: 'params', customKey: 'sex' })).toEqual([{ sex: { name: 'params1' } }])
     })
-
     it("数组自定义多字段", () => {
         expect(customKey(origin, [{ key: "name", customKey: "key" }, { key: "params", customKey: "value" }])).toEqual([{ key: "a", value: { name: 'params1' } }])
     })
