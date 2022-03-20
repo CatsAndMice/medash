6c73bd1d52ddcff53f5919f853753b1903c601fa
CatsAndMice <lihai_js@163.com>
2022-03-20 20:16:20 +0800

    docs: objToMap文档修改

--- docs/v3/Object/objToMap.md
+++ docs/v3/Object/objToMap.md
@@ -1,14 +1,17 @@
 ```ts
-
+medash.objToMap(value: Object): Map<string, any> 
 ```
-
+纯对象转化成`Map`对象类型
 
 **参数**  
-1. `value`(any): 
+1. `value`(Object): 对象
   
 **返回**  
-        
+`Map<string, any>`: `Map`对象        
   
 **例子**  
 
-<me-embed>const {} = require("medash");</me-embed>
+<me-embed>const {objToMap} = require("medash");
+const obj = { one: 1, two: 2 };
+const map = objToMap(obj);
+console.log(map)</me-embed>
--- test/Object/objToMap.test.ts
+++ test/Object/objToMap.test.ts
@@ -2,10 +2,10 @@ import { each, isFunc, objToMap } from "../../main";
 
 describe("objToMap test", () => {
     it("objToMap", () => {
-        const obj = { 1: 1, 2: 2 };
+        const obj = { 1: 1, 2: 2, one: 1 };
         const map = objToMap(obj);
         expect(isFunc(map.get)).toBeTruthy();
-        each(obj, (key,value) => {
+        each(obj, (key, value) => {
             expect(map.get(key)).toBe(value);
         })
     })
