7ac0e38ea6054895899459822668f7be653d4ef0
CatsAndMice <lihai_js@163.com>
2022-05-26 21:54:33 +0800

    chore: 修改createAvatar方法

--- src/Functions/createAvatar.ts
+++ src/Functions/createAvatar.ts
@@ -1,28 +1,27 @@
 import isBrowser from "./isBrowser"
 import isEmpty from "./isEmpty"
 type configType = {
-    bg?:string,
-    w?:number,
-    h?:number,
-    fillStyle?:string,
-    textAlign?:string,
-    
+    bg?: string,
+    w?: number,
+    h?: number,
+    font?: string,
+    fillStyle?: string,
 }
-export default (name: string,config?:configType) => {
+export default (name: string, config?: configType) => {
     if (!isBrowser()) return
     if (isEmpty(name)) return
     const bg = 'RGB(51, 150, 251)'
-    const w = 96
-    const h = 96
+    const w = config.w || 96
+    const h = config.h || 96
     const canvas = document.createElement("canvas")
     canvas.width = w
     canvas.height = h
     const ctx = canvas.getContext("2d")
-    ctx.fillStyle = bg
-    ctx.fillRect(0,0,w,h)
-    ctx.font = 'bold 35px 苹方'
-    ctx.fillStyle = '#FFF'
-    ctx.textAlign = 'center'
+    ctx.fillStyle = config.fillStyle || bg
+    ctx.fillRect(0, 0, w, h)
+    ctx.font = config.font || 'bold 35px 苹方'
+    ctx.fillStyle = config.fillStyle || '#FFF'
+    ctx.textAlign =  'center'
     ctx.textBaseline = 'middle'
     ctx.fillText(name.slice(-2), w / 2, h / 2)
     return canvas.toDataURL()
