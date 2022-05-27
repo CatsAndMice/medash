9e320af0c39f114c90606765db6b2fb736c05e0a
CatsAndMice <lihai_js@163.com>
2022-05-27 08:37:47 +0800

    chore: 修改createAvatar打包报错

--- src/Functions/createAvatar.ts
+++ src/Functions/createAvatar.ts
@@ -1,28 +1,37 @@
+// @ts-nocheck
 import isBrowser from "./isBrowser"
 import isEmpty from "./isEmpty"
+
 type configType = {
-    bg?: string,
-    w?: number,
-    h?: number,
-    font?: string,
-    fillStyle?: string,
+    bg: string
+    w: number
+    h: number
+    font: string
+    fillStyle: string
 }
-export default (name: string, config?: configType) => {
+const options = {
+    w: 96,
+    h: 96,
+    bg: 'RGB(51, 150, 251)',
+    font: 'bold 35px 苹方',
+    fillStyle: '#FFF'
+}
+export default (name: string, config: configType = options) => {
     if (!isBrowser()) return
     if (isEmpty(name)) return
-    const bg = 'RGB(51, 150, 251)'
-    const w = config.w || 96
-    const h = config.h || 96
+    const bg = config.bg
+    const w = config.w
+    const h = config.h
     const canvas = document.createElement("canvas")
     canvas.width = w
     canvas.height = h
     const ctx = canvas.getContext("2d")
-    ctx.fillStyle = config.fillStyle || bg
+    ctx.fillStyle = bg
     ctx.fillRect(0, 0, w, h)
-    ctx.font = config.font || 'bold 35px 苹方'
-    ctx.fillStyle = config.fillStyle || '#FFF'
-    ctx.textAlign =  'center'
+    ctx.font = config.font
+    ctx.fillStyle = config.fillStyle
+    ctx.textAlign = 'center'
     ctx.textBaseline = 'middle'
     ctx.fillText(name.slice(-2), w / 2, h / 2)
-    return canvas.toDataURL()
+    return canvas.toDataURL() as string
 }
